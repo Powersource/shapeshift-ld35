19d098e2b7000652906ca7f085e31e0c5103fc60
Jacob Karlsson <jacob.karlsson95@gmail.com>
2016-04-16 15:36:47 +0200

    Make the player cubes spawn

---
+++ Assets/Main.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using AssemblyCSharp;
+
+public class Main : MonoBehaviour
+{
+	public GameObject playerCube;
+
+	Player player;
+
+	// Use this for initialization
+	void Start ()
+	{
+		player = new Player(playerCube);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+
+	void FixedUpdate ()
+	{
+		
+	}
+}
---
+++ Assets/Player.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+
+namespace AssemblyCSharp
+{
+	public class Player
+	{
+
+		GameObject container;
+		GameObject[,] cubes;
+
+		public Player (GameObject playerCube)
+		{
+			container = new GameObject ("PlayerContainer");
+			cubes = new GameObject[3,3];
+			for (int y = 0; y < 3; y++) {
+				for (int x = 0; x < 3; x++) {
+					cubes [x, y] = GameObject.Instantiate (playerCube);
+					cubes [x, y].transform.parent = container.transform;
+					cubes [x, y].transform.localPosition = new Vector2 (x, y);
+				}
+			}
+		}
+	}
+}
+
