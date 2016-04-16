b72986d62982eee27c7b512cf8c9031019c1569a
Jacob Karlsson <jacob.karlsson95@gmail.com>
2016-04-16 18:53:13 +0200

    Make the player able to move

--- Assets/Main.cs
+++ Assets/Main.cs
@@ -5,13 +5,18 @@ using AssemblyCSharp;
 public class Main : MonoBehaviour
 {
 	public GameObject playerCube;
+	public GameObject simpleEnemy;
 
 	Player player;
 
 	// Use this for initialization
 	void Start ()
 	{
-		player = new Player(playerCube);
+		player = new Player (playerCube);
+		GameObject enemy = GameObject.Instantiate (simpleEnemy);//, new Vector3(0,-10,0),Quaternion.identity);
+		enemy.transform.position = new Vector2 (-5f, 0);
+		enemy.GetComponent<Rigidbody2D> ().velocity = Vector2.right;
+		enemy.transform.rotation = Quaternion.LookRotation (new Vector3 (0, 0, 1));
 	}
 	
 	// Update is called once per frame
@@ -22,6 +27,6 @@ public class Main : MonoBehaviour
 
 	void FixedUpdate ()
 	{
-		
+		player.move ();
 	}
 }
--- Assets/Player.cs
+++ Assets/Player.cs
@@ -8,12 +8,19 @@ namespace AssemblyCSharp
 	{
 
 		GameObject container;
+		Rigidbody2D containerRB;
 		GameObject[,] cubes;
+		float playerSpeed;
 
 		public Player (GameObject playerCube)
 		{
+			playerSpeed = 5f;
+
 			container = new GameObject ("PlayerContainer");
-			cubes = new GameObject[3,3];
+			container.AddComponent<Rigidbody2D> ();
+			containerRB = container.GetComponent<Rigidbody2D> ();
+
+			cubes = new GameObject[3, 3];
 			for (int y = 0; y < 3; y++) {
 				for (int x = 0; x < 3; x++) {
 					cubes [x, y] = GameObject.Instantiate (playerCube);
@@ -21,6 +28,15 @@ namespace AssemblyCSharp
 					cubes [x, y].transform.localPosition = new Vector2 (x, y);
 				}
 			}
+		}
+
+		public void move ()
+		{
+			containerRB.velocity = Vector2.zero;
+			float hDir = Input.GetAxisRaw ("Horizontal");
+			float vDir = Input.GetAxisRaw ("Vertical");
+			Vector2 dir = new Vector2 (hDir, vDir).normalized;
+			containerRB.velocity = dir * playerSpeed;
 		}
 	}
 }
