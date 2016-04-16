f5e5a4024fe63610e2560a826ebce6e99727c6c0
Jacob Karlsson <jacob.karlsson95@gmail.com>
2016-04-16 23:44:23 +0200

    Add restart screen

--- Assets/Main.cs
+++ Assets/Main.cs
@@ -1,17 +1,22 @@
 ﻿using UnityEngine;
 using System.Collections;
 using AssemblyCSharp;
+using UnityEngine.UI;
 
 public class Main : MonoBehaviour
 {
 	public GameObject playerCube;
 	public GameObject simpleEnemy;
 
+	GameObject canvas;
+
 	Player player;
 
 	// Use this for initialization
 	void Start ()
 	{
+		canvas = GameObject.Find ("DeadCanvas");
+		canvas.SetActive (false);
 		player = new Player (playerCube);
 		GameObject enemy = GameObject.Instantiate (simpleEnemy);//, new Vector3(0,-10,0),Quaternion.identity);
 		enemy.transform.position = new Vector2 (-5f, 0);
@@ -28,6 +33,11 @@ public class Main : MonoBehaviour
 	void FixedUpdate ()
 	{
 		player.move ();
+		if (Input.GetButton ("Jump")) {
+			Time.timeScale = 0.25f;
+		} else {
+			Time.timeScale = 1.0f;
+		}
 	}
 
 	public void hurt(GameObject cube, Collision2D collision){
@@ -41,5 +51,16 @@ public class Main : MonoBehaviour
 
 	void gameOver (){
 		Debug.Log ("DEAD");
+//		Text text = canvas.AddComponent<Text> ();
+//		text.text = "You dead";
+//		// I'm setting the "size" to 12 but the font size ends up being 14
+//		text.font = UnityEngine.Font.CreateDynamicFontFromOSFont("Arial", 12);
+//		Button button = canvas.AddComponent<Button> ();
+		Time.timeScale = 0;
+		canvas.SetActive(true);
+	}
+
+	public void restart(){
+		Debug.Log("Restart");
 	}
 }
