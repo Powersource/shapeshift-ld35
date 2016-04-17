d15839207a3390886ada6f811cfd090316cf42d1
Jacob Karlsson <jacob.karlsson95@gmail.com>
2016-04-17 16:28:10 +0200

    Make the player able to rotate

--- Assets/Main.cs
+++ Assets/Main.cs
@@ -2,26 +2,28 @@
 using System.Collections;
 using AssemblyCSharp;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Main : MonoBehaviour
 {
 	public GameObject playerCube;
 	public GameObject simpleEnemy;
 
-	GameObject canvas;
+	GameObject deadCanvas;
 
 	Player player;
 
 	// Use this for initialization
 	void Start ()
 	{
-		canvas = GameObject.Find ("DeadCanvas");
-		canvas.SetActive (false);
+		Time.timeScale = 1.0f;
+		deadCanvas = GameObject.Find ("DeadCanvas");
+		deadCanvas.SetActive (false);
 		player = new Player (playerCube);
-		GameObject enemy = GameObject.Instantiate (simpleEnemy);//, new Vector3(0,-10,0),Quaternion.identity);
-		enemy.transform.position = new Vector2 (-5f, 0);
-		enemy.GetComponent<Rigidbody2D> ().velocity = Vector2.right;
-		enemy.transform.rotation = Quaternion.LookRotation (new Vector3 (0, 0, 1));
+		GameObject enemy = GameObject.Instantiate (simpleEnemy);
+		enemy.transform.position = new Vector2 (0, 10f);
+		enemy.GetComponent<Rigidbody2D> ().velocity = Vector2.down;
+		enemy.transform.Rotate(new Vector3(0,0,180f));
 	}
 	
 	// Update is called once per frame
@@ -51,16 +53,17 @@ public class Main : MonoBehaviour
 
 	void gameOver (){
 		Debug.Log ("DEAD");
-//		Text text = canvas.AddComponent<Text> ();
+//		Text text = deadCanvas.AddComponent<Text> ();
 //		text.text = "You dead";
 //		// I'm setting the "size" to 12 but the font size ends up being 14
 //		text.font = UnityEngine.Font.CreateDynamicFontFromOSFont("Arial", 12);
-//		Button button = canvas.AddComponent<Button> ();
+//		Button button = deadCanvas.AddComponent<Button> ();
 		Time.timeScale = 0;
-		canvas.SetActive(true);
+		deadCanvas.SetActive(true);
 	}
 
 	public void restart(){
 		Debug.Log("Restart");
+		SceneManager.LoadScene ("scene0");
 	}
 }
--- Assets/Player.cs
+++ Assets/Player.cs
@@ -10,38 +10,52 @@ namespace AssemblyCSharp
 		GameObject container;
 		Rigidbody2D containerRB;
 		GameObject[,] cubes;
+		int cubeSide;
 		float playerSpeed;
+		float rotateSpeed;
+		// float cubeWidth;
 
 		public Player (GameObject playerCube)
 		{
+			cubeSide = 3;
 			playerSpeed = 5f;
+			rotateSpeed = 1f;
 
 			container = new GameObject ("PlayerContainer");
-			container.AddComponent<Rigidbody2D> ();
-			containerRB = container.GetComponent<Rigidbody2D> ();
+			containerRB = container.AddComponent<Rigidbody2D> ();
 
-			cubes = new GameObject[3, 3];
-			for (int y = 0; y < 3; y++) {
-				for (int x = 0; x < 3; x++) {
+			cubes = new GameObject[cubeSide, cubeSide];
+			for (int y = 0; y < cubeSide; y++) {
+				for (int x = 0; x < cubeSide; x++) {
 					cubes [x, y] = GameObject.Instantiate (playerCube);
 					cubes [x, y].transform.parent = container.transform;
-					cubes [x, y].transform.localPosition = new Vector2 (x, y);
+					cubes [x, y].transform.localPosition = new Vector2 (-cubeSide/2 + x, -cubeSide/2 + y);
 				}
 			}
+			// I do this after adding the cubes. If I do it before
+			// then the cubes don't shrink.
+			container.transform.localScale = Vector3.one / 2;
+			// cubeWidth = cubes [0, 0].GetComponent<BoxCollider2D> ().size.x;
 		}
 
 		public void move ()
 		{
+			// Setting velocity
 			containerRB.velocity = Vector2.zero;
 			float hDir = Input.GetAxisRaw ("Horizontal");
 			float vDir = Input.GetAxisRaw ("Vertical");
 			Vector2 dir = new Vector2 (hDir, vDir).normalized;
 			containerRB.velocity = dir * playerSpeed;
+
+			// Rotating
+			float rotateDir = Input.GetAxisRaw ("Aim");
+			container.transform.Rotate(new Vector3(0, 0, -rotateDir * rotateSpeed));
 		}
 
-		public bool isAlive(){
+		public bool isAlive ()
+		{
 			int aliveCount = 0;
-			foreach(GameObject cube in cubes){
+			foreach (GameObject cube in cubes) {
 				if (cube.activeSelf) {
 					aliveCount++;
 				}
@@ -50,4 +64,3 @@ namespace AssemblyCSharp
 		}
 	}
 }
-
